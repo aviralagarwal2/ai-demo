1a583b42894490af361fe32fb2462dbdbb281908
Aviral Agarwal <aviral0201@fmail.com>
2025-03-16 14:09:15 +0530

    cors added

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,29 @@ connect();
 
 const app = express();
 
-app.use(cors());
+const allowedOrigins = process.env.PORT;
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", process.env.PORT);
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+  });
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    credentials: true,
+    optionsSuccessStatus: 200 
+};
+
+app.use(cors(corsOptions))
+
+
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
